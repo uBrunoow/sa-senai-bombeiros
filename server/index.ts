ce3a92df8854404f23019864bea5205cf260b00f
Bruno Werner <101356770+uBrunoow@users.noreply.github.com>
2023-08-10 09:40:19 -0300

    Co-authored-by: LucasHitoshi <LucasHitoshi@users.noreply.github.com> Co-authored-by: Matheus Felipe de Freitas Borges <Mfelipe-Borges0@users.noreply.github.com> Commit

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,13 @@
 import fastify from 'fastify'
-
-const app = fastify() // Dar para a const app todas as informações do Fastify
+const app = fastify()  // Dar para a const app todas as informações do Fastify
 
 app
   .listen({
-    port: 3333, // Porta para rodar na Aplicação WEB
-    host: '0.0.0.0', // Porta para rodar na Aplicação APP
+    port: 3333,  // Porta para rodar na Aplicação WEB
+    host: '0.0.0.0',  // Porta para rodar na Aplicação APPs
   })
   .then(() => {
-    console.log('🔥🚒 HTTP server running on port http://localhost:3333') // Ao rodar o server aparecer a seguinte mensagem
+    console.log(
+      '🔥🚒🤷‍♂️ HTTP server running on port http://localhost:3333'
+    )  // Ao rodar o server aparecer a seguinte mensagem
   })
