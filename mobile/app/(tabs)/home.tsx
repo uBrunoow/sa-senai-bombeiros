097f1b41e126d0a78e3ed1d3c4db6ab3814bbe26
LucianoKohler <108994222+LucianoKohler@users.noreply.github.com>
2023-09-18 12:15:43 -0300

    teste

--- mobile/app/(tabs)/home.tsx
+++ mobile/app/(tabs)/home.tsx
@@ -1,45 +1,41 @@
-import { View, Text, TouchableOpacity } from 'react-native'
-import NOARLogo from '../../src/assets/logo-noar.svg'
-import Icon from '@expo/vector-icons/Feather'
-import { useSelector } from 'react-redux'
-import { RootState } from '../../src/stores/stores'
+import { View, Text, TouchableOpacity, ScrollView } from 'react-native'
+// import Logobvg from '../../src/assets/logobvg.png'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import React from 'react'
 
 export default function App({ navigation }) {
-  const isLoggedIn = useSelector((state: RootState) => state.auth.token !== '')
+  const { bottom, top } = useSafeAreaInsets()
 
-  const handleButtonClick = () => {
-    if (isLoggedIn) {
-      navigation.navigate('ocorrencia')
-    } else {
-      navigation.navigate('login')
-    }
-  }
   return (
-    <View className=" flex-1 items-center justify-between">
-      <View className="h-[67px] w-full bg-[#A00E00]" />
-      <View className=" h-[425px] w-full items-center justify-between p-6">
-        <NOARLogo className=" h-[147px] w-[148px] shrink-0" />
-        <Text className="text-[32px] font-normal leading-[32px] text-[#202020]">
-          Bem Vindo(a)!
+    <ScrollView
+      className="flex-1"
+      contentContainerStyle={{ paddingBottom: bottom, paddingTop: top }}
+    >
+      <View className=" flex-1 items-center justify-between">
+        {/* <Logobvg /> */}
+        <Text className="m-5 text-2xl font-bold">Bem Vindo(a)!</Text>
+        <Text className="text-center text-lg font-bold text-red-600 ">
+          Associação de Serviços Sociais Voluntários de Guaramirim
         </Text>
-
-        <Text className=" text-center text-[14px] font-normal leading-[22.4px] text-[#000]">
+        <Text className="m-4 text-justify">
           Bravos heróis que arriscam suas vidas todos os dias para proteger
-          comunidades e salvar vidas.Os bombeiros estão na linha de frente em
-          situações de emergência, e para ajuda-los é necassario um meio mais
-          ágil de relatórios
+          comunidades e salvar vidas. Os bombeiros estão na linha de frente em
+          situações de emergência, e para ajuda-los é necessário um meio mais
+          ágil de relatórios.
         </Text>
       </View>
-      <View className="relative h-[67px] w-full items-center bg-[#A00E00]">
-        <View className="absolute top-[-50px] h-[100px] w-[100px] items-center justify-center rounded-full bg-offwhite">
-          <TouchableOpacity
-            className="h-[66px] w-[66px] items-center justify-center rounded-full bg-[#A00E00]"
-            onPress={handleButtonClick}
-          >
-            <Icon name="plus" size={50} color="#fff" />
-          </TouchableOpacity>
+      <View>
+        <TouchableOpacity>
+          <View className="m-auto w-2/6 rounded-lg bg-red-600 p-1">
+            <Text className="color-white text-center text-lg">Relatorio</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+      <View className="h-[67px] w-full bg-[#A00E00]">
+        <View>
+          <Text>BOTAR AS REDES AQUI</Text>
         </View>
       </View>
-    </View>
+    </ScrollView>
   )
 }
