bb8225f0e52ea5974c21a01168f54ef893432704
Bruno Werner <brunowerner7@gmail.com>
2023-10-03 19:19:21 -0300

    [~feat] Criada uma pasta especificamente para modais.

--- mobile/app/(tabs)/FInalizacao/finalizacao.tsx
+++ mobile/app/(tabs)/FInalizacao/finalizacao.tsx
@@ -24,6 +24,7 @@ import { calculateAnamnesisCompleteness } from '../../../src/utils/calculateAnam
 import { useSelector } from 'react-redux'
 import { RootState } from '../../../src/redux/stores/stores'
 import findUser from '../../../src/api/findUser'
+import FInalizacaoModal from '../../modal/FInalizacaoModal'
 
 const Finalizacao = () => {
   const [selected, setSelected] = React.useState('')
@@ -221,27 +222,7 @@ const Finalizacao = () => {
                 className="rounded-[7px] bg-white p-4 "
               >
                 <View className="relative flex-row items-center justify-center">
-                  <View className="w-[320px]">
-                    <Text className="w-[300px] text-left text-[20px] font-bold">
-                      Coloque o nome do responsável pelo preenchimento
-                    </Text>
-                    <Text className="mt-3 text-[#979797b0]">
-                      (Caso o nome não for preenchido o responsável será o nome
-                      da pessoa com a conta logada.)
-                    </Text>
-                    <View>
-                      <Text className="mt-5">Nome:</Text>
-                      <TextInput
-                        className="mt-2 items-center justify-between rounded-[7px] border-width1 border-preto p-[10px]"
-                        placeholder="Digite um nome"
-                      />
-                    </View>
-                    <Pressable className=" mt-10 w-[100px] items-center justify-center rounded-[7px] bg-[#F23030] p-3">
-                      <Text className="text-[18px] font-bold uppercase text-white">
-                        Salvar
-                      </Text>
-                    </Pressable>
-                  </View>
+                  <FInalizacaoModal />
                   <Pressable
                     onPress={() => setChangeResponsable(false)}
                     className="absolute right-[-5px] top-1"
