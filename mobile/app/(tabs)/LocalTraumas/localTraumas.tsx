54ccfa74b50b2360169cb07a3abbc6022580d735
lucashitoshi <lucasogawarec@gmail.com>
2023-10-16 12:28:27 -0300

    InfoPaciente front is done. Started LocalTraumas.
    Co-authored-by: Matheus Felipe de Freitas Borges <Mfelipe-Borges0@users.noreply.github.com>

--- mobile/app/(tabs)/LocalTraumas/localTraumas.tsx
+++ mobile/app/(tabs)/LocalTraumas/localTraumas.tsx
@@ -1,12 +1,22 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import Header from '../../components/Header'
+import { styles as s } from '@app/styles/boxShadow'
+import { MaterialIcons } from '@expo/vector-icons'
 
 export default function LocalTraumas() {
   return (
     <View>
       <Header />
-      <Text>Local Traumas</Text>
+      <View className=" mt-[34px] flex-row items-center justify-center">
+        <MaterialIcons name="person" size={24} color="#A00e00" />
+        <Text className="ml-[10px] text-[20px] font-medium leading-[20px]">
+          Local Traumas
+        </Text>
+      </View>
+      <View style={s.boxShadow}>
+        <Text>Mano</Text>
+      </View>
     </View>
   )
 }
