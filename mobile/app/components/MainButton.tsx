9586c0df0dc924a4188edc8f9b8b3746f9c592da
Luciano K. da Silva <lucianokohlerdasilva@gmail.com>
2023-10-25 20:42:36 -0300

    progresso no login, arrumando o title do botao

--- mobile/app/components/MainButton.tsx
+++ mobile/app/components/MainButton.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { ActivityIndicator, Pressable, Text, View } from 'react-native'
+import { ActivityIndicator, Text } from 'react-native'
 import { Button } from 'native-base'
 
 type MainButtonProps = {
@@ -11,16 +11,18 @@ type MainButtonProps = {
 export default function MainButton(props: MainButtonProps) {
   return (
     <Button
+      className="mx-auto rounded-md bg-red-700"
       onPress={props.onPress}
       colorScheme={'red'}
-      mx={'auto'}
       mb={8}
       w={'75%'}
     >
       {props.isLoading ? (
         <ActivityIndicator size="large" color="#ffffff" />
       ) : (
-        <Text className="text-xl font-bold text-white">{props.innerText}</Text>
+        <Text className="text-center text-xl font-bold text-white">
+          {props.innerText}
+        </Text>
       )}
     </Button>
   )
