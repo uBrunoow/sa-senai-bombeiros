8682bad9e60fbdc92bf336eb026adfb5010128bb
uBrunoow <brunowerner7@gmail.com>
2023-11-09 08:07:59 -0300

    [feat] Expiration date

--- mobile/src/lib/api.ts
+++ mobile/src/lib/api.ts
@@ -1,12 +1,12 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'http://10.3.76.176:3333',
+  baseURL: 'http://10.3.79.132:3333',
   headers: {
     'Content-Type': 'application/json',
   },
   // IP DO LUCIANO: 10.3.77.141
-  // IP DO BRUNOW 10.3.76.234 && IP CASA DO BRUNOW 192.168.0.9
+  // IP DO BRUNOW 10.3.79.132 && IP CASA DO BRUNOW 192.168.0.9
   // IP DA AUPI: 172.16.1.39
   // IP DO POKAWA 10.3.78.121
   // IP DA CASINHA DO LU 10.0.0.185
