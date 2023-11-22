de51bec1d349b9816418b41343551434f880ea25
uBrunoow <brunowerner7@gmail.com>
2023-11-22 11:04:19 -0300

    [FEAT] NAO SEI PQ EU FIZ AQ MAS TD BEM Ver mais e ver menos na intro

--- mobile/src/lib/api.ts
+++ mobile/src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'http://10.3.78.108:3333',
+  baseURL: 'http://10.3.79.132:3333',
   headers: {
     'Content-Type': 'application/json',
   },
