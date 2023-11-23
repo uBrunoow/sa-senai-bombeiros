7705197f0bc339a35e89cc3198379b94f03a7c4c
uBrunoow <brunowerner7@gmail.com>
2023-11-23 09:44:03 -0300

    [fix] verify cinematic avaliation

--- mobile/src/utils/verifyCinematicAvaliation.tsx
+++ mobile/src/utils/verifyCinematicAvaliation.tsx
@@ -1,7 +1,7 @@
 export const verifyCinematicAvaliation = (value: boolean) => {
   if (value === true) {
-    return '<div className="radioType"></div>'
+    return <div className="radioType"></div>
   } else {
-    return '<div className="noRadio"></div>'
+    return <div className="noRadio"></div>
   }
 }
